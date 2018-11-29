d9ff659cf9d336a143203629eec0c81fae4c54a3
Fouré <benoit.foure@accenture.com>
2018-11-29 09:23:35 +0100

    codelyzer compliance

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [FormsModule],
       declarations: [
         AppComponent
       ],
@@ -22,10 +24,83 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('katabowling');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should initialize score to 0', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to katabowling!');
+    const app = fixture.debugElement.componentInstance;
+    const score = app.getScore();
+    expect(score).toEqual(0);
   });
+
+  it('should score the number of hits in the frame in case of no strike or spare', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    app.line = [[3, 2, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]];
+    const score = app.getScore();
+    expect(score).toEqual(5);
+  });
+
+  it('should add the number of hits of the next 2 rolls to the frame score in case of strike', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    app.line = [[10, 0, 0], [3, 2, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]];
+    const score = app.getScore();
+    expect(score).toEqual(20);
+  });
+
+  it('should add the number of hits of the 3 rolls in case of strike at the last frame', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    app.line = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [10, 10, 10]];
+    const score = app.getScore();
+    expect(score).toEqual(30);
+  });
+
+  it('should add the number of hits of the next roll to the frame score in case of spare', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    app.line = [[5, 5, 0], [3, 2, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]];
+    const score = app.getScore();
+    expect(score).toEqual(18);
+  });
+
+  it('should add the number of hits of the 3 rolls in case of spare at the last frame', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    app.line = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [5, 5, 10]];
+    const score = app.getScore();
+    expect(score).toEqual(20);
+  });
+
+  it('should give 300 point to a full strikes series', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    app.line = [[10, 0, 0], [10, 0, 0], [10, 0, 0], [10, 0, 0], [10, 0, 0], [10, 0, 0], [10, 0, 0], [10, 0, 0], [10, 0, 0], [10, 10, 10]];
+    const score = app.getScore();
+    expect(score).toEqual(300);
+  });
+
+  it('should give 150 point to a series of 5-hit tries', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    app.line = [[5, 5, 0], [5, 5, 0], [5, 5, 0], [5, 5, 0], [5, 5, 0], [5, 5, 0], [5, 5, 0], [5, 5, 0], [5, 5, 0], [5, 5, 5]];
+    const score = app.getScore();
+    expect(score).toEqual(150);
+  });
+
+  it('should give 90 point to a series of 9+0 frames', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    app.line = [[9, 0, 0], [9, 0, 0], [9, 0, 0], [9, 0, 0], [9, 0, 0], [9, 0, 0], [9, 0, 0], [9, 0, 0], [9, 0, 0], [9, 0, 0]];
+    const score = app.getScore();
+    expect(score).toEqual(90);
+  });
+
+  it('should rest the line when a new game is launched', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    app.line = [[9, 0, 0], [9, 0, 0], [9, 0, 0], [9, 0, 0], [9, 0, 0], [9, 0, 0], [9, 0, 0], [9, 0, 0], [9, 0, 0], [9, 0, 0]];
+    app.reset();
+    expect(app.line.length).toEqual(0);
+  });
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,63 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'katabowling';
+  line = [];
+  currentFrame = [0, 0, 0];
+  end = false;
+  maxlength = 10;
+  maxpins = 10;
+
+  addLine() {
+    this.line.push(this.currentFrame);
+    this.currentFrame = [0, 0, 0];
+    if (this.line.length === this.maxlength) {
+      this.end = true;
+    }
+  }
+
+  reset() {
+    this.line = [];
+    this.currentFrame = [0, 0, 0];
+    this.end = false;
+  }
+
+  getScore() {
+    let total = 0;
+    this.line.forEach(frame  => {
+      // Is it the last frame ?
+      if (this.line.indexOf(frame) === this.maxlength - 1) {
+        total = total + frame[0] + frame[1] + frame[2];
+      }
+      // It is not the last frame
+      else {
+        // case 1: strike
+        if (frame[0] === this.maxpins) {
+          const nextFrame = this.line.indexOf(frame) + 1;
+          // if next frame is the last:
+          if (nextFrame + 1 === this.maxlength) {
+            total = total + 10 + this.line[nextFrame][0] + this.line[nextFrame][1];
+          }
+          // if next frame is a strike
+          else if (this.line[nextFrame][0] === this.maxpins) {
+            total = total + 10 + this.line[nextFrame][0] + this.line[nextFrame + 1][0];
+          }
+          // default
+          else {
+            total = total + 10 + this.line[nextFrame][0] + this.line[nextFrame][1];
+          }
+        }
+        // case 2: spare
+        else if (frame[0] + frame[1] === this.maxpins) {
+          const nextFrame = this.line.indexOf(frame) + 1;
+          total = total + 10 + this.line[nextFrame][0];
+        }
+        // case 3: default
+        else {
+          total = total + frame[0] + frame[1];
+        }
+
+      }
+    });
+    return total;
+  }
 }
